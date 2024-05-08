1ac66e24de3bb5f15617cb6e13962b1e4797b65d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-05-08 23:24:07 +0300

    new items

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -5,7 +5,7 @@ export const NewsItem = ({title, description, src, url}) => {
   <img src={src} style={{height:"200px", width:"360px"}} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title.slice(0,50)}</h5>
-    <p className="card-text">{description}</p>
+    <p className="card-text">{description?description.slice(0,90):"News Item"}</p>
     <a href={url} className="btn btn-primary">Read More</a>
   </div>
 </div>
