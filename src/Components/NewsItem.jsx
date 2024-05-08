5710e4f5d1841b3e1683f83276da22ac648b89ed
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-05-08 23:00:24 +0300

    news items

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -2,11 +2,11 @@
 export const NewsItem = ({title, description, src, url}) => {
   return (
         <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={src} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
+    <p className="card-text">{Description}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
   </div>
 </div>
    
