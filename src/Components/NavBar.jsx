af246972985fa8a8d2697d91ce7259e3fd59f45d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-05-08 18:27:29 +0300

    news item set up

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
     <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">NewsMagazine</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 
 export const NewsBoard = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect (() => {
+        let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=${(import.meta.env.VITE_API_KEY)}';
+        fetch(url).then(response => response.json()).then(data => setArticles(data.articles));
+    },[])
   return (
     <div>
-        <h2 className="text-center">Latest <span className="">News</span></h2>
+        <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
     </div>
   )
 }
