ad4432aa0391cec27d79b8953c9fb2e4016b6fc3
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-05-08 23:21:21 +0300

    news item display fix

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,14 +1,16 @@
 
 export const NewsItem = ({title, description, src, url}) => {
   return (
-        <div className="card" style={{maxWidth: "345px"}}>
-  <img src={src} className="card-img-top" alt="..."/>
+        <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3" style={{maxWidth: "345px"}}>
+  <img src={src} style={{height:"200px", width:"360px"}} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{Description}</p>
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description}</p>
     <a href={url} className="btn btn-primary">Read More</a>
   </div>
 </div>
    
   )
 }
+
+export default NewsItem
